7b6f8b9f46d4f934c328521a1f3a5b32b510d472
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-06-30 22:31:19 -0300

    Adição exercício do cálculo do raio

---
+++ Course/Ex02/Program.cs
@@ -0,0 +1,22 @@
+﻿//Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro 
+//casas decimais conforme exemplos.
+//Fórmula da área: area = π.raio2
+//Considere o valor de π = 3.1415
+using System;
+using System.Globalization;
+
+
+namespace Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o raio do círculo:");
+            double r = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture
+                );
+            double area = Math.PI * (r * r);
+            Console.WriteLine($"A={area.ToString("F4", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
