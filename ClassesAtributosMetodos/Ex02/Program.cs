f921dafbbec584be90010838c5fe8a6851f105da
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-02 00:04:01 -0300

    Solução do segundo exercício sobre Atributos e Métodos

--- ClassesAtributosMetodos/Ex02/Program.cs
+++ ClassesAtributosMetodos/Ex02/Program.cs
@@ -1,5 +1,6 @@
 ﻿//Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário 
 //médio dos funcionários
+using System.Globalization;
 
 namespace Ex02
 {
@@ -7,9 +8,21 @@ namespace Ex02
     {
         static void Main(string[] args)
         {
+            Funcionario primeiroFunc = new Funcionario();
+            Funcionario segundoFunc = new Funcionario();
+            double salarioMedio = 0;
+            
+            Console.Write("Dados do primeiro funcionario:\r\nNome:");
+            primeiroFunc.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            primeiroFunc.salario = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Dados do segundo funcionario:\r\nNome:");
+            segundoFunc.nome = Console.ReadLine();
+            Console.Write("Salário: ");
+            segundoFunc.salario = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            Console.WriteLine("Dados do primeiro funcionario:");
-            Console.WriteLine("Dados do segundo funcionario:");
+            salarioMedio = ((primeiroFunc.salario + segundoFunc.salario) / 2);
+            Console.WriteLine($"Salário médio: {salarioMedio.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
