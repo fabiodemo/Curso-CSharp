62158578cd417a75bcf36ef49412b468e9567daa
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-01 23:36:39 -0300

    Adição das soluções de exercicios sobre Atributos e Métodos

---
+++ ClassesAtributosMetodos/Ex01/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoaUm = new Pessoa();
+            Pessoa pessoaDois = new Pessoa();
+
+            Console.Write("Dados da primeira pessoa:\r\nNome: ");
+            pessoaUm.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoaUm.idade = int.Parse(Console.ReadLine());
+            Console.Write("Dados da segunda pessoa:\r\nNome: ");
+            pessoaDois.nome = Console.ReadLine();
+            Console.Write("Idade: ");
+            pessoaDois.idade = int.Parse(Console.ReadLine());
+
+            if (pessoaUm.idade > pessoaDois.idade)
+            {
+                Console.WriteLine($"Pessoa mais velha: {pessoaUm.nome}");
+            }
+            else if (pessoaDois.idade > pessoaUm.idade) 
+            {
+                Console.WriteLine($"Pessoa mais velha: {pessoaDois.nome}");
+            }
+            else
+            {
+                Console.WriteLine("As duas pessoas tem a mesma idade!");
+            }
+        }
+    }
+}
---
+++ ClassesAtributosMetodos/Ex02/Program.cs
@@ -0,0 +1,15 @@
+﻿//Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário 
+//médio dos funcionários
+
+namespace Ex02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Dados do primeiro funcionario:");
+            Console.WriteLine("Dados do segundo funcionario:");
+        }
+    }
+}
