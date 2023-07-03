059638473327f1c9c14911a7ec0bdb36f75ef490
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-02 23:04:40 -0300

    Alteração nos exemplos de classes

--- ClassesAtributosMetodos/ClassesAtributosMetodos/Program.cs
+++ ClassesAtributosMetodos/ClassesAtributosMetodos/Program.cs
@@ -39,26 +39,56 @@ namespace ClassesAtributosMetodos
 
             // ---------------------------------------------------------
 
-            Triangulo x, y;
-            x = new Triangulo();
-            y = new Triangulo();
+            //Triangulo x, y;
+            //x = new Triangulo();
+            //y = new Triangulo();
+
+            //Console.WriteLine("Digite as medidas do triângulo X:");
+            //x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //double p = (x.A + x.B + x.C) / 2;
+            //double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
+
+
+
+            //Console.WriteLine("Digite as medidas do triângulo X:");
+            //y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            //p = (y.A + y.B + y.C) / 2;
+            //double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
+
+            //Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            //Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+            //if (areaX > areaY)
+            //{
+            //    Console.WriteLine("maior área: X");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("maior área: Y");
+            //}
+
+            // ---------------------------------------------------------
+
+            Triangulo x = new Triangulo();
+            Triangulo y = new Triangulo();
 
             Console.WriteLine("Digite as medidas do triângulo X:");
             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            double p = (x.A + x.B + x.C) / 2;
-            double areaX = Math.Sqrt(p * (p - x.A) * (p - x.B) * (p - x.C));
-
-
 
             Console.WriteLine("Digite as medidas do triângulo X:");
             y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-            p = (y.A + y.B + y.C) / 2;
-            double areaY = Math.Sqrt(p * (p - y.A) * (p - y.B) * (p - y.C));
-            
+
+            double areaX = x.Area();
+
+            double areaY = y.Area();
+
             Console.WriteLine("Área de X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("Área de Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
             if (areaX > areaY)
--- ClassesAtributosMetodos/ClassesAtributosMetodos/Triangulo.cs
+++ ClassesAtributosMetodos/ClassesAtributosMetodos/Triangulo.cs
@@ -1,4 +1,6 @@
-﻿namespace ClassesAtributosMetodos
+﻿using System;
+
+namespace ClassesAtributosMetodos
 {
     class Triangulo
     {
@@ -6,5 +8,11 @@
         public double B;
         public double C;
 
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
     }
 }
