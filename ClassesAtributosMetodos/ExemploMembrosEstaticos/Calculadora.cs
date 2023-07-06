a2d679a4bd7da6fb686a1a01e52255d7e65dce43
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-06 11:14:49 -0300

    Exemplo de Membros Estáticos

---
+++ ClassesAtributosMetodos/ExemploMembrosEstaticos/Calculadora.cs
@@ -0,0 +1,16 @@
+﻿namespace ExemploMembrosEstaticos
+{
+    class Calculadora
+    {
+        public static double Pi = 3.14;
+        public static double Circunferencia(double raio)
+        {
+            return 2.0 * Pi * raio;
+        }
+
+        public static double Volume(double r)
+        {
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3.0);
+        }
+    }
+}
--- ClassesAtributosMetodos/ExemploMembrosEstaticos/Program.cs
+++ ClassesAtributosMetodos/ExemploMembrosEstaticos/Program.cs
@@ -8,7 +8,15 @@ namespace ExemploMembrosEstaticos
         static void Main(string[] args)
         {
             Console.Write("Digite o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double circ = Calculadora.Circunferencia(raio);
+            Console.WriteLine($"Circunferencia: {circ.ToString("F2", CultureInfo.InvariantCulture)}");
 
+
+            double volume = Calculadora.Volume(raio);
+            Console.WriteLine($"Circunferencia: {volume.ToString("F2", CultureInfo.InvariantCulture)}");
+            Console.WriteLine($"{Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
