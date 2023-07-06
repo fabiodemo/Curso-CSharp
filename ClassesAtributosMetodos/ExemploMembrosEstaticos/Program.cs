74b5c6e8d4607879a7f7527e1a27b19d628ca271
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-06 11:02:31 -0300

    Exemplo de Membros Estáticos

---
+++ ClassesAtributosMetodos/ExemploMembrosEstaticos/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExemploMembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor do raio: ");
+
+        }
+    }
+}
