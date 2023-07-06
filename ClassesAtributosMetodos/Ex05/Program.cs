ec77287cf743d8a5f6e796192059388154c493d0
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-05 22:59:11 -0300

    Finalização exercício 5

--- ClassesAtributosMetodos/Ex05/Program.cs
+++ ClassesAtributosMetodos/Ex05/Program.cs
@@ -4,6 +4,8 @@
 //do funcionário com base em uma porcentagem dada (somente o
 //salário bruto é afetado pela porcentagem) e mostrar novamente os
 //dados do funcionário. Use a classe projetada abaixo.
+using System;
+using System.Globalization;
 
 namespace Ex05;
 
@@ -11,6 +13,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Funcionario func = new Funcionario();
+
+        Console.Write("Nome: ");
+        func.Nome = Console.ReadLine();
+        Console.Write("Salário Bruto: ");
+        func.SalarioBruto = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.Write("Imposto: ");
+        func.Imposto = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+        Console.WriteLine($"Funcionário: {func.Nome}, $ {func.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}");
+
+        Console.Write("Digite a porcentagem para aumentar o salário: ");
+        func.AumentarSalario(Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+
+        Console.WriteLine($"Dados atualizados: {func.Nome}, $ {func.SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}");
+
     }
 }
