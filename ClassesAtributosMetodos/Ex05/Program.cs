4a7a43bcbca19dcf31521d750d26de09084d6d45
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-05 22:24:00 -0300

    Inserção do titulo dos exercicios

--- ClassesAtributosMetodos/Ex05/Program.cs
+++ ClassesAtributosMetodos/Ex05/Program.cs
@@ -1,4 +1,11 @@
-﻿namespace Ex05;
+﻿//Fazer um programa para ler os dados de um funcionário (nome,
+//salário bruto e imposto). Em seguida, mostrar os dados do
+//funcionário (nome e salário líquido). Em seguida, aumentar o salário
+//do funcionário com base em uma porcentagem dada (somente o
+//salário bruto é afetado pela porcentagem) e mostrar novamente os
+//dados do funcionário. Use a classe projetada abaixo.
+
+namespace Ex05;
 
 class Program
 {
