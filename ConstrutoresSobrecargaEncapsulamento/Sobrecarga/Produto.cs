7f38c296883aeb1756aa62c760f9a9c915bf2897
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-08 17:16:09 -0300

    Exemplo de Sobrecargas

---
+++ ConstrutoresSobrecargaEncapsulamento/Sobrecarga/Produto.cs
@@ -0,0 +1,52 @@
+﻿using System.Globalization;
+
+namespace Sobrecarga
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public Produto(string nome, double preco)
+        { 
+            Nome = nome;
+            Preco = preco;
+            //Quantidade = 0; // Nesse caso a quantidade já será 0, mas se quisermos podemos setar um valor diferente 
+        }
+
+        public Produto()
+        {
+        }
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
