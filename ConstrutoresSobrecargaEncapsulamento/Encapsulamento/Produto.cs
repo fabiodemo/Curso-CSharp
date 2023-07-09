90d35e42593c7abb8edab81d2467591fb92074e9
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-09 16:13:27 -0300

    Inicialização Encapsulamento

---
+++ ConstrutoresSobrecargaEncapsulamento/Encapsulamento/Produto.cs
@@ -0,0 +1,46 @@
+﻿using System.Globalization;
+
+namespace Encapsulation
+{
+    class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public Produto()
+        {
+        }
+
+            public Produto(string Nome, double Preco, int Quantidade)
+        {
+            this.Nome = Nome;
+            this.Preco = Preco;
+            this.Quantidade = Quantidade;
+        }
+
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return Nome
+            + ", $ "
+            + Preco.ToString("F2", CultureInfo.InvariantCulture)
+            + ", "
+            + Quantidade
+            + " unidades, Total: $ "
+            + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ConstrutoresSobrecargaEncapsulamento/Encapsulamento/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+namespace Encapsulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Produto p = new Produto("TV", 500.0, 10);
+            //Console.WriteLine(p);
+
+            Produto p = new Produto
+            {
+                Nome = "TV",
+                Preco = 500.00,
+                Quantidade = 20
+            };
+
+            // Podemos alterar a quantidade conforme quisermos
+            p.Quantidade = -10;
+
+        }
+    }
+}
