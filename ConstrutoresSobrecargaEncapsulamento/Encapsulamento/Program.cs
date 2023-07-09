52a62cebe1d520ab1f7dac66423ec4472a6a7826
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-09 16:26:46 -0300

    Exemplo Encapsulamento

--- ConstrutoresSobrecargaEncapsulamento/Encapsulamento/Program.cs
+++ ConstrutoresSobrecargaEncapsulamento/Encapsulamento/Program.cs
@@ -6,18 +6,33 @@ namespace Encapsulation
     {
         static void Main(string[] args)
         {
-            //Produto p = new Produto("TV", 500.0, 10);
+            Produto p = new Produto("tv", 500.0, 10);
             //Console.WriteLine(p);
 
-            Produto p = new Produto
-            {
-                Nome = "TV",
-                Preco = 500.00,
-                Quantidade = 20
-            };
+            //Produto p = new Produto
+            //{
+            //    _nome = "TV",
+            //    _preco = 500.00,
+            //    _quantidade = 20
+            //};
 
-            // Podemos alterar a quantidade conforme quisermos
-            p.Quantidade = -10;
+            // Podemos alterar a quantidade conforme quisermos, isso não é ideal
+            //p.Quantidade = -10;
+            // Só devemos alterar o valor da quantidade através dos métodos da classe
+
+            // Com o encapsulamento, torna-se impossível até acessar e imprimir
+            //Console.WriteLine(p._nome);
+
+            Console.WriteLine(p.GetNome());
+
+            p.SetNome("TV 4K");
+            Console.WriteLine(p.GetNome());
+
+            // Assegurar manter um estado consistente no objeto, não permitindo mudar o nome para somente um caracter
+            p.SetNome("T");
+            Console.WriteLine(p.GetNome());
+            Console.WriteLine(p.GetPreco());
+            Console.WriteLine(p.GetQuantidade());
 
         }
     }
