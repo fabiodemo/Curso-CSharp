4a4a69cf5aaba03c763d61543161ce203a873104
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-09 22:01:53 -0300

    Exemplo Properties

---
+++ ConstrutoresSobrecargaEncapsulamento/Properties/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+
+namespace Properties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            // Properties
+            Produto p = new Produto("tv", 500.0, 10);
+
+            p.Nome = "TV 4K";
+            Console.WriteLine(p.Nome);
+            Console.WriteLine(p.Preco);
+            Console.WriteLine(p.Quantidade);
+
+            p.Nome = "T";
+            Console.WriteLine(p.Nome);
+            //p.Preco = 20.00; // Erro, pois só teremos o get nesse caso
+            */
+
+            // Auto Properties
+        }
+    }
+}
