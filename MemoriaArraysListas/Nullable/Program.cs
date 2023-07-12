e113ae7db5f6a4baa7b92c60d299f3db6c478beb
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-11 22:14:05 -0300

    Exemplos Nullable

--- MemoriaArraysListas/Nullable/Program.cs
+++ MemoriaArraysListas/Nullable/Program.cs
@@ -6,7 +6,34 @@ namespace Nullable
     {
         static void Main(string[] args)
         {
-            double x = null;
+            //double x = null; // Non-nullable
+            //Nullable<double> x = null;
+            double? x = null;
+            double? y = 10.0;
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            //Console.WriteLine(x.Value); //  Nullable object must have a value.
+            if (x.HasValue) Console.WriteLine(x.Value);
+            else Console.WriteLine("X é nulo!");
+
+            if (y.HasValue) Console.WriteLine(y.Value);
+            else Console.WriteLine("Y é nulo!");
+
+            // Coalescência nula
+            // Se o primeiro valor for nulo, pega o segundo valor
+            //double? a = null;
+
+            double? a = null;
+            double? b = 10;
+
+            double c = x ?? 5;
+            double d = y ?? 5;
+            Console.WriteLine(c);
+            Console.WriteLine(d);
         }
     }
 }
