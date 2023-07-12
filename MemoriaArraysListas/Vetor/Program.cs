588381227708a256ed094d0ee164a65afdc96e73
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-12 20:12:39 -0300

    Inserção de segundo exemplo sobre vetores

--- MemoriaArraysListas/Vetor/Program.cs
+++ MemoriaArraysListas/Vetor/Program.cs
@@ -6,6 +6,8 @@ namespace Vetor
     {
         static void Main(string[] args)
         {
+            /*
+            // Exemplo 1
             int n = int.Parse(Console.ReadLine());
             double[] vet = new double[n];
 
@@ -22,6 +24,27 @@ namespace Vetor
             double avg = sum / n;
 
             Console.WriteLine($"AVERAGE HEIGHT {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+            */
+
+            int n = int.Parse(Console.ReadLine());
+
+            Product[] vect = new Product[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                vect[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0.0;
+            for (int i = 0; i < n;  i++)
+            {
+                sum += vect[i].Price;
+            }
+
+            double avg = sum / n;
+            Console.WriteLine($"AVERAGE PRICE = {avg.ToString("F2", CultureInfo.InvariantCulture)}");
         }
     }
 }
