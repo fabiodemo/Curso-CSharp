958b53665849b60217af2c2f7ab19df2cd94f5a9
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-12 20:05:33 -0300

    Inserção de exemplo sobre vetores

---
+++ MemoriaArraysListas/Vetor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Globalization;
+
+namespace Vetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double[] vet = new double[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                vet[i] = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+            double sum = 0.0;
+            for (int i = 0; i < n; i++)
+            {
+                sum += vet[i];
+            }
+
+            double avg = sum / n;
+
+            Console.WriteLine($"AVERAGE HEIGHT {avg.ToString("F2", CultureInfo.InvariantCulture)}");
+        }
+    }
+}
