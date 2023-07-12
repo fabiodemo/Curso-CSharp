0fae3067f2d7c56240e9007949020e3b3082534a
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-11 21:44:35 -0300

    Remoção de um projeto

---
+++ Nullable/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Drawing;
+
+namespace Nullable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point;
+            p.X = 10;
+            p.Y = 10;
+            Console.WriteLine(p);
+        }
+    }
+}
