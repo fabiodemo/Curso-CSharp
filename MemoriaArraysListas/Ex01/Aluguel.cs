f389821ab0d0afec7103c8fb3b8516384c1114f9
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-13 19:50:20 -0300

    Exercício com vetores

---
+++ MemoriaArraysListas/Ex01/Aluguel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex01
+{
+    class Aluguel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        
+        public Aluguel() { }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Email}";
+        }
+    }
+}
---
+++ MemoriaArraysListas/Ex01/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluguel[] quartos = new Aluguel[10];
+
+            Console.Write("How Many rooms will be rented? ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Rent #{i}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                int room = 0;
+                do
+                {
+                    Console.Write("Room: ");
+                    room = int.Parse(Console.ReadLine());
+                    if (quartos[room] != null)
+                    {
+                        Console.WriteLine("Room already occupied! Select another room ");
+                    }
+                } while (quartos[room] != null);
+                Console.WriteLine(room);
+                quartos[room] = new Aluguel { Name = name, Email = email };
+            }
+
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i < 10; i++)
+            {
+                if (quartos[i] != null)
+                {
+                    Console.WriteLine($"{i}: {quartos[i].ToString()}");
+                }
+            }
+
+        }
+    }
+}
