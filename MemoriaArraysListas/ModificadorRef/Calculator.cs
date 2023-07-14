2c4aa197516e7498acd0ae3ba89a1fc096e6c6cd
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-13 21:17:37 -0300

    Exemplo ref e out

---
+++ MemoriaArraysListas/ModificadorRef/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificadorRef
+{
+    class Calculator
+    {
+        public static void Triple(ref int x)
+        {
+            x = x * 3;
+        }
+
+        public static void TripleNew(int origin, out int result)
+        {
+            result = origin * 3;
+        }
+    }
+}
---
+++ MemoriaArraysListas/ModificadorRef/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ModificadorRef
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ref e out são code smells
+            int a = 10;
+            Calculator.Triple(ref a); // ref exige que a variável seja iniciada
+            Console.WriteLine(a);
+
+            int triple;
+            Calculator.TripleNew(a, out triple);
+            Console.WriteLine(triple);
+        }
+    }
+}
