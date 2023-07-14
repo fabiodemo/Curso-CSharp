5bce53fbb1ddc8771561039a6636d3432fc75cd4
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-13 21:49:46 -0300

    Exemplo foreach

---
+++ MemoriaArraysListas/Foreach/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Foreach;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] vect = new string[] { "Maria", "Alex", "Roberto" };
+        for (int i = 0; i < vect.Length; i++) {
+            Console.WriteLine($"{vect[i]}");
+        }
+
+        Console.WriteLine("---------------");
+        foreach (string s in vect)
+        {
+            Console.WriteLine(s);
+        }
+    }
+}
