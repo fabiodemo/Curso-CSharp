ec62eda73edcd7f443763ccff3b3ccc8aef67269
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-13 21:06:34 -0300

    Exemplo params

---
+++ MemoriaArraysListas/Params/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Params
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s1 = Calculator.Sum(2, 3);
+            int s2 = Calculator.Sum(2, 4, 3);
+            int s3 = Calculator.Sum(2, 4, 3, 5);
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+        }
+    }
+}
