e8b3c964fb0a58503fdabdb2adbd30f5a4fc669b
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-07-14 22:13:26 -0300

    Exemplo de listas

--- MemoriaArraysListas/List/Program.cs
+++ MemoriaArraysListas/List/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace List
+﻿using System.Collections.Generic;
+namespace List
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> list = new List<string>();
+
+            List<string> list2 = new List<string> { "Maria", "Alex", "Bob" };
+
+            Console.WriteLine(list);
+            Console.WriteLine(list2);
         }
     }
 }
