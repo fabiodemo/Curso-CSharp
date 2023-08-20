c5a546ec9a6f5b4e9315dc7dd32d6af6ce173a1d
Fábio Demo da Rosa <39056032+fabiodemo@users.noreply.github.com>
2023-08-19 23:00:38 -0300

    Exemplo de Matriz

---
+++ MemoriaArraysListas/Matriz/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Matriz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] mat = new double[2,3];
+
+            Console.WriteLine($"{mat}");
+
+            Console.WriteLine(mat.Length);
+
+            Console.WriteLine(mat.Rank);
+
+            Console.WriteLine(mat.GetLength(0));
+            Console.WriteLine(mat.GetLength(1));
+        }
+    }
+}
