e04df77d08c97d6de2cc8ee8a354648e687e9120
fabiodemo <faberdemo@gmail.com>
2023-09-11 20:22:34 -0300

    Criação de um novo projeto para Tópicos Especiais do C#

---
+++ 4_Topicos_Especiais/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _4_Topicos_Especiais
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
