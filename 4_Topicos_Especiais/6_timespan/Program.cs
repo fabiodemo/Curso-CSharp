c5900a34d82f7b9ca41a83f72b97cbaf978f5c25
fabiodemo <faberdemo@gmail.com>
2023-09-13 20:16:48 -0300

    Reorganização do diretório

---
+++ 4_Topicos_Especiais/6_timespan/Program.cs
@@ -0,0 +1,26 @@
+﻿
+namespace _6_timespan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // store the duration in the format of ticks
+            TimeSpan t1 = new TimeSpan(0, 1, 30);
+            Console.WriteLine(t1);
+            Console.WriteLine(t1.Ticks);
+
+            TimeSpan t2 = new TimeSpan();
+            TimeSpan t3 = new TimeSpan(90000000L);
+            TimeSpan t4 = new TimeSpan(2, 11, 21, 21);
+            TimeSpan t5 = new TimeSpan(2, 11, 21, 21, 212);
+            Console.WriteLine(t2);
+            Console.WriteLine(t3);
+            Console.WriteLine(t4);
+            Console.WriteLine(t5);
+
+            Console.WriteLine(TimeSpan.FromDays(1.5));
+
+        }
+    }
+}
