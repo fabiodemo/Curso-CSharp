1d7adbf324194b31f67b20437ca849c00fddfb00
fabiodemo <faberdemo@gmail.com>
2023-09-23 14:57:24 -0300

    Inicio de exemplos de COmposition

---
+++ 5_Enumeracao_Composicao/Ex01/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ex01Composition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
