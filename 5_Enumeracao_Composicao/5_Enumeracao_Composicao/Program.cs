288cc7ff90b776c5ce3341606580bcf2ce9ac9bd
fabiodemo <faberdemo@gmail.com>
2023-09-23 10:45:38 -0300

    Exemplos de Enums

---
+++ 5_Enumeracao_Composicao/5_Enumeracao_Composicao/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace _5_Enumeracao_Composicao
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ 5_Enumeracao_Composicao/Enum/Program.cs
@@ -0,0 +1,26 @@
+﻿using Enum.Entities;
+using Enum.Entities.Enums;
+using System;
+
+namespace Enum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order
+            {
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment,
+            };
+
+
+            string txt = OrderStatus.PendingPayment.ToString();
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+            
+            Console.WriteLine(order);
+            Console.WriteLine(txt);
+        }
+    }
+}
