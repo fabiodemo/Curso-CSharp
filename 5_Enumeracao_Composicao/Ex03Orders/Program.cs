f88bc4dd21f89f8640146373cae7badcae946b61
fabiodemo <faberdemo@gmail.com>
2023-09-28 23:24:19 -0300

    Annotations and console writes of the exercise

--- 5_Enumeracao_Composicao/Ex03Orders/Program.cs
+++ 5_Enumeracao_Composicao/Ex03Orders/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter client data: ");
+            Console.Write("Name: ");
+            Console.Write("Email: ");
+            Console.Write("Birth date (DD/MM/YYYY): ");
+            Console.WriteLine("Enter order data: ");
+            Console.Write("Status: ");
+            Console.Write("How many items do this order have? ");
+            // for
+            Console.WriteLine($"Enter #i item data: ");
+            Console.Write("Product name: ");
+            Console.Write("Product price: ");
+            Console.Write("Quantity: ");
+
+            // print results
+
+
+
+
         }
     }
 }
