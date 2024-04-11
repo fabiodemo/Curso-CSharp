d354f554fb9e06090a28a26e7a368925c8138559
fabiodemo <faberdemo@gmail.com>
2024-04-10 21:44:37 -0300

    Sobreposição ou Sobrescrita

---
+++ 6_Heranca_Polimorfismo/SobreposicaoVirtualOverrideBase/Entities/SavingAccount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobreposicaoVirtualOverrideBase.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double InterestRate {  get; protected set; }
+
+        public SavingsAccount() { }
+
+        public SavingsAccount(int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpdateBalance(double balance)
+        {
+            Balance += Balance * InterestRate;
+        }
+
+        public override void Withdraw(double amount)
+        {
+            //Balance -= amount;
+            base.Withdraw(amount);
+            Balance -= 2;
+        }
+    }
+}
