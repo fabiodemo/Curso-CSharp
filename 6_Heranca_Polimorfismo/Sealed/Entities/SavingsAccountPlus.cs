eb2cf914caa0f200ec5a2e567855792afb03ea8b
fabiodemo <faberdemo@gmail.com>
2024-04-15 23:04:36 -0300

    Selando métodos e classes

---
+++ 6_Heranca_Polimorfismo/Sealed/Entities/SavingsAccountPlus.cs
@@ -0,0 +1,15 @@
+﻿using SobreposicaoVirtualOverrideBase.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sealed.Entities
+{
+    internal class SavingsAccountPlus : SavingsAccount
+    {
+        // Proibido porque o método está selado.
+        public override void Withdraw()
+    }
+}
