27d94156e35d45dea76ef553fdbd27256ad34d6f
fabiodemo <faberdemo@gmail.com>
2024-04-17 23:08:06 -0300

    Exercicio 01

---
+++ 6_Heranca_Polimorfismo/Ex01/Program.cs
@@ -0,0 +1,68 @@
+﻿/*
+ Uma empresa possui funcionários próprios e terceirizados.
+Para cada funcionário, deseja-se registrar nome, horas
+trabalhadas e valor por hora. Funcionários terceirizados
+possuem ainda uma despesa adicional.
+
+O pagamento dos funcionários corresponde ao valor da hora
+multiplicado pelas horas trabalhadas, sendo que os
+funcionários terceirizados ainda recebem um bônus
+correspondente a 110% de sua despesa adicional.
+
+Fazer um programa para ler os dados de N funcionários (N
+fornecido pelo usuário) e armazená-los em uma lista. Depois
+de ler todos os dados, mostrar nome e pagamento de cada
+funcionário na mesma ordem em que foram digitados.
+Construa o programa conforme projeto ao lado. Veja
+exemplo na próxima página.
+ */
+using Ex01.Entities;
+using System.Globalization;
+
+namespace Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+
+            Console.Write("Enter the number of employees: ");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Employee #{i} data:");
+                Console.Write("Outsourced (y/n)? ");
+                char ch = char.Parse(Console.ReadLine());
+                
+                Console.Write("Name: ");
+                String name = Console.ReadLine();
+
+                Console.Write("Hours: ");
+                int hours = int.Parse(Console.ReadLine());
+
+                Console.Write("Value per hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if (ch == 'y')
+                {
+                    Console.Write("Additional charge: ");
+                    double additionalCharge = Double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    employees.Add(new OutsourcedEmployee(name, hours, valuePerHour, additionalCharge));
+                }
+                else
+                {
+                    employees.Add(new Employee(name, hours, valuePerHour));
+                }
+
+                Console.WriteLine();
+                Console.WriteLine("Payments: ");
+                foreach (Employee emp in employees)
+                {
+                    Console.WriteLine(emp.Name + " - $ " + emp.Payment().ToString("F2", CultureInfo.InvariantCulture));
+                }
+            }
+        }
+    }
+}
