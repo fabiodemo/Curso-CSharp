8abcbf1990903a7a159c406eba321df5348bdfe7
fabiodemo <faberdemo@gmail.com>
2024-04-19 22:27:49 -0300

    Métodos abstratos

--- 6_Heranca_Polimorfismo/AbstractClass/Program.cs
+++ 6_Heranca_Polimorfismo/AbstractClass/Program.cs
@@ -1,20 +1,36 @@
 ﻿using System;
-using Course.Entities;
+using AbstractClass.Entities;
+using System.Globalization;
 
-namespace Course
+namespace AbstractClass
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Account acc1 = new Account(1001, "Alex", 500.0);
-            Account acc2 = new SavingsAccount(1002, "Anna", 500.0, 0.01);
+            // Não pode instanciar do tipo Abstract
+            //Account acc1 = new Account(1001, "Alex", 500.0);
+            List<Account> accounts = new List<Account>();
+            accounts.Add(new SavingsAccount(1001, "Alex", 500.0, 0.01));
+            accounts.Add(new BusinessAccount(1002, "Maria", 500.0, 400.0));
+            accounts.Add(new SavingsAccount(1003, "Bob", 500.0, 0.01));
+            accounts.Add(new BusinessAccount(1004, "Ana", 500.0, 500.0));
 
-            acc1.Withdraw(10.0);
-            acc2.Withdraw(10.0);
+            double sum = 0.0;
+            foreach (Account account in accounts)
+            {
+                sum += account.Balance;
+            }
+            Console.WriteLine($"Total balance: {sum.ToString("F2", CultureInfo.InvariantCulture)}");
 
-            Console.WriteLine(acc1.Balance);
-            Console.WriteLine(acc2.Balance);
+            foreach(Account account in accounts)
+            {
+                account.Withdraw(10.0);
+            }
+            foreach (Account account in accounts)
+            {
+                Console.WriteLine($"Updated balance for account {account.Number}: {account.Balance.ToString("F2", CultureInfo.InvariantCulture)}");
+            }
         }
     }
 }
